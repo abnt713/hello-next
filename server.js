5388c01681c3521e3147068dc17c6f29535ac7d7
Alison Bento <alisonbnt@gmail.com>
2019-03-30 23:50:06 -0300

    :sparkles: Finishes lesson 7

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app
 
     server.get('/p/:id', (req, res) => {
       const actualPage = '/post'
-      const queryParams = { title: req.params.id }
-      app.render(req, res, actualPage , queryParams)
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
     })
 
     server.get('*', (req, res) => {
